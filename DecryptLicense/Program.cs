ab7b50f8ec77264e24e58d1187c24460866542b3
Edvin He <edhe@microsoft.com>
2016-11-18 14:41:26 +0800

    Update per the requirements

--- DecryptLicense/Program.cs
+++ DecryptLicense/Program.cs
@@ -25,7 +25,7 @@ namespace DecryptLicense
         {
           FileUtil fu = new FileUtil();
 
-          List<byte> originalLicense = getBytes(@"C:\Users\edhe.FAREAST\Documents\My Received Files\license");
+          List<byte> originalLicense = getBytes(@"C:\Users\edhe.FAREAST\Desktop\sharing\LSZ\LSZ\myfolder\edvinhe\license_guangbo");
           String originalStr = Encoding.UTF8.GetString(fu.decompressFile(originalLicense.ToArray()));
           Console.WriteLine(originalStr);
 
@@ -36,7 +36,7 @@ namespace DecryptLicense
       static void printMachineFileContent()
         {
           FileUtil fileUtil = new FileUtil();
-          List<byte> list = getBytes(@"C:\Users\edhe.FAREAST\Documents\My Received Files\machine");
+          List<byte> list = getBytes(@"C:\Users\edhe.FAREAST\Desktop\sharing\LSZ\LSZ\myfolder\machine");
           string input = Encoding.UTF8.GetString(fileUtil.decompressFile(list.ToArray()));
           JavaScriptSerializer serializer = new JavaScriptSerializer();
           MachineInfo mf = serializer.Deserialize<MachineInfo>(input);
@@ -60,50 +60,18 @@ namespace DecryptLicense
             JavaScriptSerializer serializer = new JavaScriptSerializer();
             MachineInfo mf = serializer.Deserialize<MachineInfo>(input);
             // We get machine info now
-            Console.WriteLine("生成简单license文件？ (yes/no)");
-            string answer = Console.ReadLine();
-            bool flag = true;
-            if(answer.Trim().Equals("yes") || answer.Trim().Equals("Yes") || answer.Trim().Equals("YES"))
-            {
-              flag = true;
-            }
-            else
-            {
-              flag = false;
-            }
+            Console.WriteLine("正在生成广播license文件和屏幕license文件......");
 
-            String deviceList = getDeviceList(mf, flag);
+            //获取屏幕PIS device list
+            String screenDeviceList = getDeviceList(mf, "PIS");
+            //获取广播PAS device list
+            String broadcastDeviceList = getDeviceList(mf, "PAS");
 
-            StringBuilder sb = new StringBuilder();
-            sb.Append("{");
-            sb.Append("\"LocationStation\":{\"StationCode\":\""+ mf.StationCode+"\",\"StationName\":\""+mf.StationName+"\"},\"DebugMode\":1,\"DeviceList\":{");
-            sb.Append(deviceList);
-            sb.Append("},\"Version\":\"1.0.0.2\",\"StartTimeValue\":\"2016-10-13 00:00:00\",\"EndTimeValue\":\"2066-01-13 23:59:59\"}");
+            // Generate PIS license file
+            generateLicensePerDevice(mf, screenDeviceList, fileUtil, machineFilePath, "PIS");
 
-            RSACryptoServiceProvider provider3 = new RSACryptoServiceProvider();
-            byte[] bytes = Encoding.UTF8.GetBytes(provider3.ToXmlString(true));
-            byte[] src = fileUtil.compressFile(Encoding.UTF8.GetBytes(sb.ToString()));
-
-            List<byte> result = new List<byte>();
-            int srcOffset = 0;
-
-            while (srcOffset < src.Length)
-            {
-              int count = ((src.Length - srcOffset) >= 100) ? 100 : (src.Length - srcOffset);
-              byte[] dst = new byte[count];
-              Buffer.BlockCopy(src, srcOffset, dst, 0, count);
-              srcOffset += 100;
-              byte[] collection = provider3.Encrypt(dst, false);
-              result.AddRange(collection);
-            }
-            byte[] array = new byte[bytes.Length + result.Count];
-            bytes.CopyTo(array, 0);
-            result.CopyTo(array, bytes.Length);
-            byte[] buffer6 = fileUtil.compressFile(array);
-
-            string newLicenFilePath = machineFilePath.Substring(0,machineFilePath.LastIndexOf("\\")) + "\\license";
-            File.WriteAllBytes(newLicenFilePath, buffer6);
-            Console.WriteLine("新的license文件所在文件路径为：  " + newLicenFilePath);
+            // Generate PAS license file
+            generateLicensePerDevice(mf, broadcastDeviceList, fileUtil, machineFilePath, "PAS");
           }
           else
           {
@@ -115,7 +83,72 @@ namespace DecryptLicense
           Console.ReadLine();
         }
 
-        static String getDeviceList(MachineInfo mf, bool simpleLicenseFile)
+
+      static void generateLicensePerDevice(MachineInfo mf, String deviceList, FileUtil fileUtil, String machineFilePath, string deviceType)
+      {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("{");
+        sb.Append("\"LocationStation\":{\"StationCode\":\"" + mf.StationCode + "\",\"StationName\":\"" + mf.StationName + "\"},\"DebugMode\":1,\"DeviceList\":{");
+        sb.Append(deviceList);
+
+        String startTimeValue = null;
+        String endTimeValue = null;
+
+        if (deviceType.Equals("PAS"))
+        {
+          startTimeValue = "2015-12-24 00:00:00";
+          endTimeValue = "2086-03-24 23:59:59";
+        }
+        else if (deviceType.Equals("PIS"))
+        {
+          startTimeValue = "2016-10-13 00:00:00";
+          endTimeValue = "2066-01-13 23:59:59";
+        }
+        sb.Append("},\"Version\":\"1.0.0.2\",\"StartTimeValue\":\"" + startTimeValue + "\",\"EndTimeValue\":\"" + endTimeValue + "\"}");
+
+        RSACryptoServiceProvider provider3 = new RSACryptoServiceProvider();
+        byte[] bytes = Encoding.UTF8.GetBytes(provider3.ToXmlString(true));
+        byte[] src = fileUtil.compressFile(Encoding.UTF8.GetBytes(sb.ToString()));
+
+        List<byte> result = new List<byte>();
+        int srcOffset = 0;
+
+        while (srcOffset < src.Length)
+        {
+          int count = ((src.Length - srcOffset) >= 100) ? 100 : (src.Length - srcOffset);
+          byte[] dst = new byte[count];
+          Buffer.BlockCopy(src, srcOffset, dst, 0, count);
+          srcOffset += 100;
+          byte[] collection = provider3.Encrypt(dst, false);
+          result.AddRange(collection);
+        }
+        byte[] array = new byte[bytes.Length + result.Count];
+        bytes.CopyTo(array, 0);
+        result.CopyTo(array, bytes.Length);
+        byte[] buffer6 = fileUtil.compressFile(array);
+
+        string newLicenFilePath = null;
+        if (deviceType.Equals("PIS"))
+        {
+          newLicenFilePath = machineFilePath.Substring(0, machineFilePath.LastIndexOf("\\")) + "\\license_屏授权";
+        }
+        else if (deviceType.Equals("PAS"))
+        {
+          newLicenFilePath = machineFilePath.Substring(0, machineFilePath.LastIndexOf("\\")) + "\\license_广播授权";
+        }
+         
+        File.WriteAllBytes(newLicenFilePath, buffer6);
+
+        if (deviceType.Equals("PIS"))
+        {
+          Console.WriteLine("新的屏授权license文件所在文件路径为：  " + newLicenFilePath);
+        }
+        else if (deviceType.Equals("PAS"))
+        {
+          Console.WriteLine("新的广播授权license文件所在文件路径为：  " + newLicenFilePath);
+        }
+      }
+        static String getDeviceList(MachineInfo mf, string device)
         {
           StringBuilder sb = new StringBuilder();
           String tempList = mf.EquipmentsXml;
@@ -125,69 +158,10 @@ namespace DecryptLicense
           Dictionary<string, List<string>> deviceModelMap = new Dictionary<string, List<string>>();
 
           XmlNodeList xmlNodes = doc.DocumentElement.SelectNodes("/NewDataSet/Table");
-          if (!simpleLicenseFile)
-          {
+            sb.Append("\"" + device +"\":[");
             foreach (XmlNode xmlNode in xmlNodes)
             {
-              List<string> ll = null;
-
-              String deviceCode = xmlNode["NUM_EQT"].InnerText;
-              String deviceID = xmlNode["IDENO_EQT"].InnerText;
-              String deviceName = xmlNode["NAME_EQT"].InnerText;
-              String deviceModel = xmlNode["MODNUM_EQT"].InnerText;
-              String deviceIP = xmlNode["IPAdd_EQT"].InnerText;
-
-              string temp = "{\"DeviceCode\":\"" + deviceCode + "\",\"DeviceId\":\"" + deviceID
-                 + "\",\"DeviceName\":\"" + deviceName + "\",\"DeviceModel\":\"" + deviceModel
-                 + "\",\"DeviceIp\":\"" + deviceIP + "\"},";
-
-              deviceModelMap.TryGetValue(deviceModel, out ll);
-              if (ll == null)
-              {
-                ll = new List<string>();
-              }
-              ll.Add(temp);
-
-              if (!deviceModelMap.Keys.Contains(deviceModel))
-              {
-                deviceModelMap.Add(deviceModel, ll);
-              }
-            }
-
-            foreach (String key in deviceModelMap.Keys)
-            {
-              List<String> dd = null;
-
-              deviceModelMap.TryGetValue(key, out dd);
-              dd[dd.Count - 1] = dd[dd.Count - 1].Substring(0, dd[dd.Count - 1].Length - 1);
-            }
-
-            foreach (string key in deviceModelMap.Keys)
-            {
-              sb.Append("\"" + key + "\":[");
-              List<string> tt = null;
-
-              deviceModelMap.TryGetValue(key, out tt);
-              if (tt != null)
-              {
-                foreach (string kk in tt)
-                {
-                  sb.Append(kk);
-                }
-              }
-              sb.Append("],");
-            }
-
-            String result = sb.ToString();
-
-            return result.Substring(0, result.Length - 1);
-          }
-          else
-          {
-            sb.Append("\"PIS\":[");
-            foreach (XmlNode xmlNode in xmlNodes)
-            {
-              if (xmlNode["MODNUM_EQT"].InnerText.Equals("PIS"))
+              if (xmlNode["MODNUM_EQT"].InnerText.Equals(device))
               {
                 String deviceCode = xmlNode["NUM_EQT"].InnerText;
                 String deviceID = xmlNode["IDENO_EQT"].InnerText;
@@ -207,7 +181,6 @@ namespace DecryptLicense
             result = result + "]";
 
             return result;
-          }
         }
         static List<byte> getBytes(string filePath)
         {
